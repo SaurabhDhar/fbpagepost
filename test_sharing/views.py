48bdfd79767ed7e8d5a9b5d9c1a287c64c5f6f73
cavidanhasanli <cavidan.hasanli@mail.ru>
2020-03-17 21:15:01 +0400

    test56

--- test_sharing/views.py
+++ test_sharing/views.py
@@ -8,12 +8,16 @@ def sharing(request):
    access_token = 'EAAEEP5iPQosBAOiKZCfA5wK0KTUIg9E2DZAKXCGKbfi8Lpd4q3fZCOrfJmsCLaasQkQTMZAlYlxNfyWNoA3u4iYEzJbSsSHNAUhYhTiXezrbdjsBk0VQOdT3Vi6ZCU5VMCN1jjEbwGpxXGbczJgQrBjLZBeLEicuAyT7mkXGhBXSZBAIUCSZAGdrfe4ZCe5TZChYhNCxgE8m6fuAGdlopp3Uue'
     
    graph = facebook.GraphAPI(access_token)
-   fields = ['name', 'posts']
-   content = graph.get_object('me', fields=fields)
+   # fields = ['name', 'posts']
+   # content = graph.get_object('me', fields=fields) # data goturmek ucun
 
-   context = {
-      'content': content
-   }
-   # content =  graph.put_object(parent_object='me', connection_name='feed',message='Hello, world')
+   graph.put_object(parent_object='me', 
+                              connection_name='feed',
+                              message='Hello, world') # data yazmaq ucun
+
+   # context = {
+   #    'content': content
+   # }
+   
     
-   return render(request, 'sharing.html', context)
+   return render(request, 'sharing.html')
