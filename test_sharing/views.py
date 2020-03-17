95dc96c0e8a6bdbcc9a35e6247e67b5a48114791
cavidanhasanli <cavidan.hasanli@mail.ru>
2020-03-17 16:39:18 +0400

    send to post

--- test_sharing/views.py
+++ test_sharing/views.py
@@ -6,13 +6,10 @@ import json
 
 def sharing(request):
     access_token = 'EAAEEP5iPQosBANYZC885XTn1vXkmWS21WRycE5wmewZANT1m68MIf39GtDrH3FXJVaTvRt6R0S09M5m8eKv7wQkyxl6IaEdCmHwuv5mZCC3nflcCoUr4wJqVJHFZBj7lD8HFpCcut595srbUiPn7eFVSwcBPjM77SIGrRj0SCcoscAgN2OvnVMbq65P3KSyr1IEZClUaOKyxxu5FHgyOx'
-
+    
     if request.method == 'POST':
         graph = facebook.GraphAPI(access_token)
-        
-        fields = ['id', 'name']
-
-        profile = graph.put_object(parent_object="me", "feed", message = "Hello World")
+        profile = graph.put_object(parent_object="me", connection_name="feed", message = "Hello World")
         print(profile)
-        
+    
     return render(request, 'sharing.html')
