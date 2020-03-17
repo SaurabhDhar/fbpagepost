9f1f43ff979d18089cd1427bca95e5667587d89b
cavidanhasanli <cavidan.hasanli@mail.ru>
2020-03-17 14:31:09 +0400

    create test sharing with facebook

---
+++ test_sharing/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TestSharingConfig(AppConfig):
+    name = 'test_sharing'
---
+++ test_sharing/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def sharing(request):
+
+    return render(request, 'sharing.html')
