a199616770c3ecd118dc929a0be50536804f77db
cavidanhasanli <cavidan.hasanli@mail.ru>
2020-03-17 19:39:44 +0400

    test56

--- test_sharing/views.py
+++ test_sharing/views.py
@@ -5,7 +5,7 @@ import json
 
 
 def sharing(request):
-    access_token = 'EAAEEP5iPQosBAPfGiq7KkHDchEjBwv3zbGlO9wFtQonvT0ccYDHPn0O6qX2XwIDhwczYhAQCmjzmsNEZAzxzVyi0K7UVZBe4yIk1lLGpMR2DbuVjvB0z0wMp27lwgzVTqDcow5lYCWyQRwiUZCbEPQZBlKAdjbgS0UrsuZBWp9i2rjEcnTtJiO3nFcKVp6OUrLuvzVMR0LHPZCt7pRTF70'
+    access_token = 'EAAEEP5iPQosBAA0I1jJ7aSVY4OL7hZBM0kTugRJM4c428ZCUUkfLY3uhxSkRpNo2txzV6ZCXP0IqJK7OYAtf72W45tBmPdQBX07P5Hv3QbY5UQz1HoVoGtTC54MzBPIWKchnyd7TPqAIobTi782gs1VNBE5GtPdRBo37BITlxwymmCyIW57fIFVa4ggWGT6uxsjKDyKyZCYaz9FMWyt5'
     
     graph = facebook.GraphAPI(access_token)
     print(graph)
