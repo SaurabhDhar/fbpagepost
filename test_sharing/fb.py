3f867aa3c617c6ea08f99886aecebe58a93e8941
cavidanhasanli <cavidan.hasanli@mail.ru>
2020-04-16 17:12:59 +0400

    sharing

--- test_sharing/fb.py
+++ test_sharing/fb.py
@@ -1,7 +1,7 @@
 import facebook
-access_token = "EAANLZCsMhezQBAOpQMRBqeReyqF9bo3cg767iRd4jRYTlwSXWh8N1iahmNMGGOEZAKgQCtQYbZBKsd9Rp0dH12M9i7hSMj3DUZBPwYYDGuisv6lEfzyVzBHCeMl8NBDod0mBZBqsfQw18052RAY1VzeI6ZACAfkdXytHNPwQ18At9QDF9x10aZBvLFyHEHrxOZCticSBuEHZAtwZDZD"
+access_token = "EAANLZCsMhezQBACKy1QMotIEUcN0EixXoju3iXp08xp2ZBLtyX6g6NJeZAw0O5MVWfAIphPYJQnwcZBqGNJVCpSZCQ50lBEXLo4OAG1wmChloWqjCNtr31J4yiqrc3kHuTaOMejEIrmXJukZC5BFXVL9AxHvKZAvT2vkgrqPZB4BHIffYuwyfNTjoiDSwxXvEy4RWuYjnADfaBr0luAoTHlbQca3bR77g9XEVZCJxNfCn0gZDZD"
 graph = facebook.GraphAPI(access_token)
 
 graph.put_object(parent_object='me',
                  connection_name='feed',
-                 message='necesen')
+                 message='Hello Guys')
